276b03c214660ad067756b9407e92bccfcb78fdb
Raine Revere <raine@cybersemics.org>
2021-05-16 11:04:54 -0600

    Upgrade to firebase v8

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import throttle from 'lodash.throttle'
 import ClickNHold from 'react-click-n-hold'
 import * as pkg from '../package.json'
 import tutorialImg from './tutorial.png'
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/database'
+import 'firebase/auth'
 
 // TODO: fix JSDOM
 // mock localStorage
